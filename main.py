6dba79b1e5398e7c4e1016d091610ba032e786ee
Joël Schaerer <joel.schaerer@laposte.net>
2015-08-23 11:34:33 +0200

    autopep8

--- main.py
+++ main.py
@@ -2,17 +2,21 @@
 import sys
 from socketIO_client import SocketIO, BaseNamespace
 
+
 class Namespace(BaseNamespace):
+
     def on_connect(self):
-        print('[Connected]',file=sys.stderr)
-    def csv_format(self,lst):
+        print('[Connected]', file=sys.stderr)
+
+    def csv_format(self, lst):
         return ",".join(str(x) for x in lst)
+
     def on_install(self, *args):
         payload = sorted(args[0].items())
-        if not hasattr(self,"headers"):
-            self.headers=[k for k,v in payload]
+        if not hasattr(self, "headers"):
+            self.headers = [k for k, v in payload]
             print(self.csv_format(self.headers))
-        print(self.csv_format([v for k,v in payload]))
+        print(self.csv_format([v for k, v in payload]))
 
 socketIO = SocketIO('stats.cyanogenmod.com', 8080, Namespace)
 socketIO.wait(seconds=1e6)
