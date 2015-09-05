38ddbea85124419b35a0cebb33615035a1f45518
Joel Schaerer <joel.schaerer@gmail.com>
2015-09-05 17:00:31 +0200

    remove useless wrapper

--- decode.py
+++ decode.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Decode model names """
 import csv
 import sys
 import re
@@ -17,20 +18,9 @@ input_reader = csv.DictReader(sys.stdin)
 field_names = list(input_reader.fieldnames)
 field_names.insert(-1, "model")
 
-def wrapper(gen):
-    """ Iterator wrapper to ignore malformed CSV exceptions """
-    while True:
-        try:
-            yield next(gen)
-        except StopIteration:
-            raise
-        except Exception as e:
-            print(e, file=sys.stderr)
-            pass
-
 writer = csv.DictWriter(sys.stdout, field_names)
 writer.writeheader()
-for i in wrapper(input_reader):
+for i in input_reader:
     i["model"] = model_dict.get(i["name"], "Unknown")
     try:
         writer.writerow(i)
