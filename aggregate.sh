67f9ef0acf6b43f66ef90719c287a430eb7028a5
Joel Schaerer <joel.schaerer@gmail.com>
2015-09-05 17:00:21 +0200

    filter on current month

--- aggregate.sh
+++ aggregate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-xzcat stats/*.csv.xz 2>/dev/null | ./decode.py | grep -o "[^,]*,[^,]*201508" | sort | uniq -c | sort -h
+xzcat stats/*.csv.xz 2>/dev/null | ./decode.py | cut -d, -f7,8,9 | grep -o ".*$(date +%Y%m)" | sort | uniq -c | sort -h
 for i in stats/*.csv.xz; do echo "$i" $(xzcat "$i" 2>/dev/null| wc -l); done
 
